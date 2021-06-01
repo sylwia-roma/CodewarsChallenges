004460edabfa4e4c4a2644402e3653bdd90caa70
sylwia-roma <44410474+sylwia-roma@users.noreply.github.com>
2021-06-01 11:58:40 +0200

    Add files via upload

---
+++ Mumbling.java
@@ -0,0 +1,54 @@
+package Mumbling;
+
+//This time no story, no theory. The examples below show you how to write function accum:
+//
+//        Examples:
+//
+//        accum("abcd") -> "A-Bb-Ccc-Dddd"
+//        accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//        accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(accum("HbideVbxncC"));
+    }
+
+    public static String accum(String s) {
+
+        String newString = "";
+        String newString2 = "";
+
+        for(int i=0; i<s.length();i++){
+            char a = s.toLowerCase().charAt(i);
+
+            String q = "";
+
+            for(int j=0; j<=i; j++) {
+                q+=a;
+
+            }
+            String firstLetter = q.substring(0,1).toUpperCase();
+            String remLetter = q.substring(1);
+            q=firstLetter+remLetter;
+            //System.out.println(q);
+            newString+=q + "-";
+        }
+
+        newString2 = newString.substring(0, newString.length()-2);
+
+        return newString2;
+
+
+//        String result = "";
+//        for(int i = 0; i != s.length(); i++)
+//        {
+//            for(int j = 1; j != i+2; j++)
+//                result = result + (j == 1 ? s.toUpperCase().charAt(i) : s.toLowerCase().charAt(i));
+//            result = result + (i != s.length()-1 ? '-' : "");
+//        }
+//        return result;
+    }
+}
